adccdcc3e9826da7405b3a5ad87a03b1fd8b777e
SamirMamede <anuarsamir@gmail.com>
2025-08-22 01:06:20 -0400

    First commit

---
+++ src/main/java/dev/java/ArchClean/ArchCleanApplication.java
@@ -0,0 +1,13 @@
+package dev.java.ArchClean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchCleanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java/ArchClean/ArchCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java.ArchClean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
