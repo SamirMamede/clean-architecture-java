ac56da16164cedccc251ac01a0b7b9087465a615
SamirMamede <anuarsamir@gmail.com>
2025-08-24 23:10:11 -0400

    feat: add use case specific for event creation

---
+++ src/main/java/dev/java/ArchClean/core/usecases/CreateEvent.java
@@ -0,0 +1,8 @@
+package dev.java.ArchClean.core.usecases;
+
+import dev.java.ArchClean.core.entities.Event;
+
+public interface CreateEvent {
+
+    public Event execute(Event event);
+}
---
+++ src/main/java/dev/java/ArchClean/core/usecases/CreateEventCase.java
@@ -0,0 +1,11 @@
+package dev.java.ArchClean.core.usecases;
+
+import dev.java.ArchClean.core.entities.Event;
+
+public class CreateEventCase implements CreateEvent {
+
+    @Override
+    public Event execute(Event event) {
+        return null;
+    }
+}
