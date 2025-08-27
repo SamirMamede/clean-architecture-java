9f80060c70464093de9fe9d8a460bc06aaf5a392
SamirMamede <anuarsamir@gmail.com>
2025-08-26 23:40:06 -0400

    feat: add event repository

---
+++ src/main/java/dev/java/ArchClean/infra/persistence/EventRepository.java
@@ -0,0 +1,6 @@
+package dev.java.ArchClean.infra.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<EventEntity, Long> {
+}
